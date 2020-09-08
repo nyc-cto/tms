970f1605eccd55a8061ef2384125887b5cdb8f49
Shannon Ladymon <shannon.ladymon@gmail.com>
2020-09-08 13:06:13 -0700

    Set up test/resource dir and golden files

--- translation_service/test/test_po_localizer.py
+++ translation_service/test/test_po_localizer.py
@@ -1,9 +1,51 @@
+import tempfile
 import unittest
 import sys
+import os
 sys.path.append('src')
 import po_localizer
 
-# TODO: Write tests for all these functions
+
+GOLDEN_IN_PO_LINES = ['msgid ""',
+                      'msgstr ""',
+                      '"Content-Type: text/plain; charset=UTF-8\\n"',
+                      '"Content-Transfer-Encoding: 8bit\\n"',
+                      '"Language: de\\n"',
+                      '"Generated-By: Serge 1.4\\n"',
+                      '#. /about',
+                      '#: File: sample.json',
+                      '#: ID: a5eea87543a1a8fad51039d72f407011',
+                      'msgid "Let\'s get things translated!"',
+                      'msgstr ""',
+                      '',
+                      'msgid "We can use machine translation to get many localizations "',
+                      '"done automatically and quickly."',
+                      'msgstr ""',
+                      '']
+
+GOLDEN_PO_MSGSTR_TEXTS = ["Let's get things translated!",
+                          "We can use machine translation to get many localizations done automatically and quickly."]
+
+GOLDEN_LOCALIZED_TEXTS = ["LET'S GET THINGS TRANSLATED!,"
+                          "WE CAN USE MACHINE TRANSLATION TO GET MANY LOCALIZATIONS DONE AUTOMATICALLY AND QUICKLY."]
+
+GOLDEN_OUT_PO_LINES = ['msgid ""',
+                       'msgstr ""',
+                       '"Content-Type: text/plain; charset=UTF-8\\n"',
+                       '"Content-Transfer-Encoding: 8bit\\n"',
+                       '"Language: de\\n"',
+                       '"Generated-By: Serge 1.4\\n"',
+                       '#. /about',
+                       '#: File: sample.json',
+                       '#: ID: a5eea87543a1a8fad51039d72f407011',
+                       'msgid "Let\'s get things translated!"',
+                       'msgstr "LET\'S GET THINGS TRANSLATED!"',
+                       '',
+                       'msgid "We can use machine translation to get many localizations "',
+                       '"done automatically and quickly."',
+                       'msgstr ""',
+                       '"WE CAN USE MACHINE TRANSLATION TO GET MANY LOCALIZATIONS DONE AUTOMATICALLY AND QUICKLY."'
+                       '']
 
 
 class TestPoLocalizer(unittest.TestCase):
@@ -12,13 +54,16 @@ class TestPoLocalizer(unittest.TestCase):
         """
         localize_po_file
             expected in: in_path, out_path, po_file, target_lang_iso, translator
-            expected out: localized po_files written to out_path
+            expected out: localized po_file written to out_path
 
         # Call localize_po_file on a file in the in_path
         # check that the file exists in the out_path dir?
         # check anything else?
         # check any edge cases?
         """
+        # TODO:  test that directory structure formation is as I expect
+        #   and also checksum of golden_in and golden_out files
+
         self.assertTrue(False)  # TODO: Fix
 
     def test_parse_po_lines(self):
