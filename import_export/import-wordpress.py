75f2d70471602325755930b8e393a4e698fb309c
Steven Young <styoung.bills@gmail.com>
2020-09-29 11:54:47 +0100

    switch to environment variables

--- import_export/import-wordpress.py
+++ import_export/import-wordpress.py
@@ -11,14 +11,13 @@ from flask import Flask, has_request_context, request
 from flask_restful import Api, Resource, reqparse
 from git import Repo
 
-
-ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+ROOT_PATH = "/var/tms-data"
 
 # TODO: Unit testing, end-to-end testing
 
 def write_post(post):
     title = "wp" + str(post['id'])
-    filename = f"{ROOT_PATH}/shared_directory/en/{title}.json"
+    filename = f"{ROOT_PATH}/source_files/en/{title}.json"
     with open(filename, 'w') as outfile:
         d = {}
         d['id'] = post['id']
@@ -30,19 +29,22 @@ def write_post(post):
 
 def get_posts():
     # TODO: change this to point to prod website
-    url = "http://localhost:8888/wp-json/wp/v2/posts"
-    user = "admin"
-    password = "vmaC YpeW CxyA DjNJ k0Rd TPpM"
+    import_url = os.environ.get('IMPORT_URL')
+    url = f"{import_url}/wp-json/wp/v2/posts"
+    user = os.environ.get('IMPORT_USER')
+    password = os.environ.get('IMPORT_PASSWORD')
     credentials = user + ':' + password
     token = base64.b64encode(credentials.encode())
-    header = {'Authorization': 'Basic ' + token.decode('utf-8')}
+    useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+    header = {'Authorization': 'Basic ' + token.decode('utf-8'), 'User-Agent': useragent, }
     response = requests.get(url, headers=header )
+    print (response.json())
     return response.json()
 
 def do_work():
     for post in get_posts():
         write_post(post)
-    utils.git_push(utils.PROJECT_ROOT_GIT_PATH, commit_message="Update shared repository: wordpress", enable_push=True)
+    #utils.git_push(utils.PROJECT_ROOT_GIT_PATH, commit_message="Update shared repository: wordpress", enable_push=True)
 
 app = Flask(__name__)
 api = Api(app)
