99f6a7982d3f78994c7a42f2b55444e68db800e5
Aditya Sridhar <aditus06@gmail.com>
2020-10-08 11:50:24 -0400

    fixes eof error.

--- setup_cron.sh
+++ setup_cron.sh
@@ -1,3 +1,3 @@
-{ printenv; echo ''; cat /var/tms/cron.config; echo '' } > /var/tms/tmpfile && mv /var/tms/tmpfile /var/tms/cron.config
+{ printenv; echo ''; cat /var/tms/cron.config; echo ""; } > /var/tms/tmpfile && mv /var/tms/tmpfile /var/tms/cron.config
 crontab /var/tms/cron.config
 service cron restart
