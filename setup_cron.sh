147986c4ac2402c234df7dd94ddf8e11e075da33
Aditya Sridhar <aditus06@gmail.com>
2020-10-08 11:42:06 -0400

    fixes typo removes wrong flag.

--- setup_cron.sh
+++ setup_cron.sh
@@ -1,3 +1,3 @@
 { printenv; echo ''; cat /var/tms/cron.config; } > /var/tms/tmpfile && mv /var/tms/tmpfile /var/tms/cron.config
-crontab -e /var/tms/cron.config
+crontab /var/tms/cron.config
 service cron restart
