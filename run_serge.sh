1fcca9054aa3ef9b33779506a74f3a104abde057
Aditya Sridhar <aditus06@gmail.com>
2020-10-09 02:20:01 -0400

    executes google-drive-export process after serge sync completes twice.

--- run_serge.sh
+++ run_serge.sh
@@ -3,3 +3,4 @@ python import_export/import-google-docs.py
 serge sync /var/tms/serge/configs/google_config.serge
 sleep 3
 serge sync /var/tms/serge/configs/google_config.serge
+python import_export/export-google-docs.py
