9e882e18fabc7182f86d3fc14fbfc8a84addddf4
matthewsilver <msilver@usdigitalresponse.org>
2020-12-11 12:52:38 -0500

    add simple ingest script and add serge google config to .gitignore

---
+++ run_ingest_sync_simple.sh
@@ -0,0 +1,5 @@
+python import_export/import-google-docs.py
+serge sync serge/configs/google_config.serge
+sleep 3
+serge sync serge/configs/google_config.serge
+python import_export/export-google-docs.py
